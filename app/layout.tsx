91edef95776820fbf3453e2d88940fdc7efd6c77
MosesLevin <moses.levin14@gmail.com>
2024-09-19 15:52:20 +0200

    added home template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
+import Navbar from './components/Navbar'
 
 export const metadata: Metadata = {
-  title: "Learning for php",
-  description: "Learning environment for another project",
-};
+  title: 'Learning for php',
+  description: 'Learning environment for another project',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body>
-        <main className="max-w-6xl mx-auto">{children}</main>
+        <main className="max-w-6xl mx-auto">
+          <Navbar />
+          {children}
+        </main>
       </body>
     </html>
-  );
+  )
 }
